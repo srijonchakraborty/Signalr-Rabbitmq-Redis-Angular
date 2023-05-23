6a3379fb905b0977654bb19a414d74e857a4acaf
srijonchak <srijonchak@live.com>
2023-05-24 03:05:25 +0600

    Starting First Notification and Redis approch

---
+++ Common/Model/Groups.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Model
+{
+    public class Groups
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
