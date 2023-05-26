bc57a568c7d47fa0eb65696c954708715b7c6317
srijonchak <srijonchak@live.com>
2023-05-26 14:55:43 +0600

    Redis and Mongodb Basic Setup completed with repositorty Pattern.

---
+++ Repository/Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(string id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task RemoveAsync(T entity);
+    }
+}
--- SignalRPractice/Program.cs
+++ SignalRPractice/Program.cs
@@ -1,4 +1,11 @@
+using Common.Model;
+using MongoDB.Driver;
+using Repository.Interface;
+using Repository.Repository;
+using SignalRPractice.ExtentionMethods;
 using SignalRPractice.Hubs;
+using SignalRPractice.RedisManager;
+using System.Linq.Expressions;
 
 namespace SignalRPractice
 {
@@ -7,27 +14,49 @@ namespace SignalRPractice
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var appSettings = builder.Configuration.GetSection("AppSettings").Get<AppSettings>();
 
-            // Add services to the container.
+            #region Cors
             builder.Services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
                 {
                     builder
-                    .WithOrigins("http://localhost:4200") 
+                    .WithOrigins(appSettings.CorsSetting[0],
+                                 appSettings.CorsSetting[1])
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
                 });
             });
 
+            #endregion
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSignalR();
+            
+            #region Register DB
+            builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(appSettings.MongoConnection.ConnectionString));
+            //Note: It Register's the IMongoDatabase for MongoDBRepository class
+            builder.Services.AddScoped(s => s.GetService<IMongoClient>().GetDatabase(appSettings.MongoConnection.ConnectionString));
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(MongoDBRepository<>));
+            #endregion
 
-            var app = builder.Build();
+            #region Redis Cache
+
+            builder.Services.AddStackExchangeRedisCache(
+            options =>
+            {
+                options.Configuration = appSettings.RedisConnection.ConnectionString;
+                //options.InstanceName = appSettings.RedisConnection.InstanceName;
+            });
+            builder.Services.AddSingleton<IRedisService, RedisService>();
+            #endregion
+
+            WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -42,8 +71,8 @@ namespace SignalRPractice
 
 
             app.MapControllers();
-            app.MapHub<NotificationHub>("/notificationhub");
+            app.SetupSignalRHubs();
             app.Run();
-        }
+        }      
     }
 }
